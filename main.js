f83182a5b86fb3b593938a752e0054be99f3157a
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-25 10:42:37 +0100

    Milestone 3

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
+
+const rowEl = document.getElementById("riga")
+
+function renderCards() {
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(data => {
+        /* console.log(data); */
+
+        data.forEach(element => {
+            /*  console.log(element); */
+            const { title, date, url } = element
+
+            const markup = `<div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <img src="${url}" alt="">
+                        <p class="image_description">${title}
+                        </p>
+                            <p class="image_description">${date}
+                        </p>
+                    </div>
+                </div>
+            </div>`
+            rowEl.innerHTML += markup
+        });
+    })
+}
+renderCards()
